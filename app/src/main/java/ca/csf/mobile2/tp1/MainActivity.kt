052ad024cea71a274572b202b17514f89180f64a
Yannick Côté <yannick.c.pro.a@gmail.com>
2020-01-14 14:13:57 -0500

    dev

--- app/src/main/java/ca/csf/mobile2/tp1/MainActivity.kt
+++ app/src/main/java/ca/csf/mobile2/tp1/MainActivity.kt
@@ -2,11 +2,25 @@ package ca.csf.mobile2.tp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var errorGroup : Group
+    private lateinit var tempGroup : Group
+    private lateinit var cityEdit : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var serverRequest : ServerRequest
+
+        errorGroup = findViewById(R.id.errorGroup)
+        tempGroup = findViewById(R.id.tempGroup)
+
+        cityEdit = findViewById(R.id.cityEditText)
+
     }
 }
---
+++ app/src/main/java/ca/csf/mobile2/tp1/ServerRequest.kt
@@ -0,0 +1,13 @@
+package ca.csf.mobile2.tp1
+
+import android.os.AsyncTask
+
+class ServerRequest (var OnSucces : (Int) -> Unit) : AsyncTask<Unit, Unit, Int>(){
+
+
+    override fun doInBackground(vararg params: Unit?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
